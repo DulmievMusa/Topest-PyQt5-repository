bc95ab31f70dc1fbaff3c12ae6d71289cfc8ff87
Musa Dulmiev <musadulmiev@gmail.com>
2023-02-12 15:05:57 +0300

    useless btn

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 import PyQt5
-print(1)
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.run)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def run(self):
+        print(1)
+        # Имя элемента совпадает с objectName в QTDesigner
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
